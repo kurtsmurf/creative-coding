30befc001bfbb106406b653cc8dbb6e619fa3422
Chris Smith <chrisronsmith@gmail.com>
2020-10-17 13:16:38 -0400

    Tweak shader

--- wono/shader.js
+++ wono/shader.js
@@ -13,18 +13,28 @@ const frag = glsl(`
   precision highp float;
 
   uniform float time;
+  uniform float aspect;
   varying vec2 vUv;
 
   void main () {
-    vec3 color = 0.5 + 0.5 * cos(time + vUv.xyx + vec3(0.0, 2.0, 4.0));
-    gl_FragColor = vec4(color, 1.0);
-  }
+		vec3 colorA = vec3(1.0, 0.0, 0.0);
+		vec3 colorB = vec3(0.0, 0.0, 1.0);
+    vec3 color = mix(colorA, colorB, vUv.y);
+    
+    vec2 center = vUv - 0.5 * mix(tan(vUv.x), tan(vUv.y), sin(time) * 2.0 + 1.0);
+    center.x *= aspect;
+    float dist = length(center);
+
+    float alpha = smoothstep(0.26, 0.25, dist);
+  	gl_FragColor = vec4(color, alpha);
+	}
 `);
 
 // Your sketch, which simply returns the shader
 const sketch = ({ gl }) => {
   // Create the shader and return it
   return createShader({
+    clearColor: 'white',
     // Pass along WebGL context
     gl,
     // Specify fragment and/or vertex shader strings
@@ -32,7 +42,9 @@ const sketch = ({ gl }) => {
     // Specify additional uniforms to pass down to the shaders
     uniforms: {
       // Expose props from canvas-sketch
-      time: ({ time }) => time
+      time: ({ time }) => time,
+      aspect: ({ width, height }) => width / height
+
     }
   });
 };
