d695f5b8cbaf19af74b148d5e0b3ed9613179458
Chris Smith <chrisronsmith@gmail.com>
2020-10-17 14:47:10 -0400

    Bring back masking

--- wono/shader.js
+++ wono/shader.js
@@ -20,22 +20,16 @@ const frag = glsl(`
   #pragma glslify: hsl2rgb = require('glsl-hsl2rgb');
 
   void main () {
-		// vec3 colorA = vec3(1.0, 0.0, 0.0);
-		// vec3 colorB = vec3(0.0, 0.0, 1.0);
-    // vec3 color = mix(colorA, colorB, vUv.y);
-    
-    // vec2 center = vUv - 0.5 * mix(tan(vUv.x), tan (vUv.y), sin(time) * 2.0 + 1.0);
-    // center.x *= aspect;
-    // float dist = length(center);
+    vec2 center = vUv - 0.5;
+    center.x *= aspect;
+    float dist = length(center);
+    float alpha = smoothstep(0.254, 0.25, dist);
 
-    // float alpha = smoothstep(0.26, 0.25, dist);
-    // gl_FragColor = vec4(color, alpha);
-    
-    float n = noise(vec3(vUv.x * aspect, vUv.y, time * 0.2));
+    float n = noise(vec3(center, time * 0.2));
 
     vec3 color = hsl2rgb(0.3 + n * 0.15, 0.5, 0.5);
 
-    gl_FragColor = vec4(color, 1.0);
+    gl_FragColor = vec4(color, alpha);
 	}
 `);
 
